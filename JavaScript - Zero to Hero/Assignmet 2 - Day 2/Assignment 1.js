54ccc0e6572da216c0f8c514bb47b53bbd73bd60
agyofficials <80948489+agyofficials@users.noreply.github.com>
2021-08-07 23:03:41 +0530

    Assignment 2 - Day 2 (Question 1 and 2)

---
+++ JavaScript - Zero to Hero/Assignmet 2 - Day 2/Assignment 1.js
@@ -0,0 +1,10 @@
+const array=["John","Peter","Salman","Arijit","Honey Singh"];
+console.log("given array:");
+console.log("array=[John,Peter,Salman,Arijit,Honey Singh]");
+console.log("The reversed array:");
+for(let i=array.length;i>=0;i--){
+
+   console.log(array[i]);
+
+
+}
---
+++ JavaScript - Zero to Hero/Assignmet 2 - Day 2/Assignment 2.js
@@ -0,0 +1,7 @@
+
+console.log("Numbers that are divisible by 5 from 1 to 100:");
+for(let i=1;i<=100;i++){
+    if(i%5 == 0){
+        console.log(i);
+    }
+}
